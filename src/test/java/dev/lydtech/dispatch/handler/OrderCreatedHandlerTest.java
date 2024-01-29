762a46e2949fd005c735f0bd64ad08e553b102e5
Jan Krol <jan.krol.legowski@gmail.com>
2024-01-29 10:40:12 +0100

    dispatch service will be now sending json to the order.dispatched topic. Tests updated

--- src/test/java/dev/lydtech/dispatch/handler/OrderCreatedHandlerTest.java
+++ src/test/java/dev/lydtech/dispatch/handler/OrderCreatedHandlerTest.java
@@ -1,5 +1,4 @@
 package dev.lydtech.dispatch.handler;
-
 import dev.lydtech.dispatch.message.OrderCreated;
 import dev.lydtech.dispatch.service.DispatchService;
 import dev.lydtech.dispatch.util.TestEventData;
@@ -7,6 +6,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static java.util.UUID.randomUUID;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -23,9 +23,19 @@ class OrderCreatedHandlerTest {
     }
 
     @Test
-    void listen() {
+    void listen_Success() throws Exception {
         OrderCreated testEvent = TestEventData.buildOrderCreatedEvent(randomUUID(), randomUUID().toString());
         handler.listen(testEvent);
+        verify(dispatchServiceMock, times(1)).process(testEvent);
+    }
+
+    @Test
+    public void listen_ServiceThrowsException() throws Exception {
+        OrderCreated testEvent = TestEventData.buildOrderCreatedEvent(randomUUID(), randomUUID().toString());
+        doThrow(new RuntimeException("Service failure")).when(dispatchServiceMock).process(testEvent);
+
+        handler.listen(testEvent);
+
         verify(dispatchServiceMock, times(1)).process(testEvent);
     }
 }
